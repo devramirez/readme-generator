2a10a0d59147c8fe60f0b2a3c7560432c2f1639e
devramirez <aramirezdev1@gmail.com>
2023-06-17 16:55:26 -0400

    added indexjs, started code

---
+++ index.js
@@ -0,0 +1,6 @@
+// require modules 
+const fs = require('fs'); 
+const inquirer = require('inquirer'); 
+
+// linking to page where the README is developed 
+const generatePage = require('./utils/generateMarkdown.js');
