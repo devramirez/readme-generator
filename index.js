55bbe2fbe9690bc3405610134332dab26d7b4126
devramirez <aramirezdev1@gmail.com>
2023-06-17 16:59:25 -0400

    started array of questions using inquirer

--- index.js
+++ index.js
@@ -2,5 +2,24 @@
 const fs = require('fs'); 
 const inquirer = require('inquirer'); 
 
-// linking to page where the README is developed 
+// linking to page where the README is generated 
 const generatePage = require('./utils/generateMarkdown.js');
+
+//array of questions for user
+const questions = () => {
+    // using inquirer to prompt questions to user
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?',
+            validate: nameInput => {
+                if (nameInput) => {
+                    return true;
+                } else {
+                    console.log('Please enter username!');
+                }
+            }
+        }
+    ])
+}
