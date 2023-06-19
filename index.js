0ad2feb58201b00443cb2000c0b404d3169d4353
devramirez <aramirezdev1@gmail.com>
2023-06-18 23:29:18 -0400

    completed prompts for readme

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // require modules 
-const fs = require('fs'); 
-const inquirer = require('inquirer'); 
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // linking to page where the README is generated 
 const generatePage = require('./utils/generateMarkdown.js');
@@ -20,92 +20,113 @@ const questions = () => {
                     console.log('Please enter username!');
                     return false;
                 }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email address!');
+                    return false;
                 }
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: 'What is your email address?',
-                validate: nameInput => {
-                    if (nameInput) {
-                        return true;
-                    } else {
-                        console.log('Please enter your email address!');
-                        return false; 
-                    }
-                }
-        
-            },
-            {
-                type: 'input',
-                name: 'title',
-                message: 'What is your project name?',
-                validate: nameInput => {
-                    if (nameInput) {
-                        return true;
-                    } else {
-                        console.log('Please enter your project name!');
-                        return false; 
-                    }
-                }
-            },
-    
-            {
-                type: 'input',
-                name: 'description',
-                message: 'Please write a short description of your project.',
-                validate: nameInput => {
-                    if (nameInput) {
-                        return true;
-                    } else {
-                        console.log('Please enter a description of your project!');
-                        return false; 
-                    }
-                }
-            },
-            {
-                type: 'input',
-                name: 'description',
-                message: 'Please write a short description of your project.',
-                validate: nameInput => {
-                    if (nameInput) {
-                        return true;
-                    } else {
-                        console.log('Please enter a description of your project!');
-                        return false; 
-                    }
-                }
-            }, 
-            {
-                type: 'list',
-                name: 'license',
-                message: 'What kind of license should your project have?',
-                choices: ['MIT', 'GNU'],
-                default: ["MIT"],
-                validate: nameInput => {
-                    if (nameInput) {
-                        return true;
-                    } else {
-                        console.log('Please choose a license!');
-                        return false; 
-                    }
-                }
-            },
-            {
-                type: 'input',
-                name: 'install',
-                message: 'What are the steps required to install your project?',
-                validate: nameInput => {
-                    if (nameInput) {
-                        return true;
-                    } else {
-                        console.log('Please enter steps required to install your project!');
-                        return false; 
-                    }
-                }
-            },
-            
+            }
 
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project name!');
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please write a short description of your project.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description of your project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please write a short description of your project.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description of your project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What kind of license should your project have?',
+            choices: ['MIT', 'GNU'],
+            default: ["MIT"],
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please choose a license!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'What are the steps required to install your project?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter steps required to install your project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How do you use this app?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a usage description!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'What command should be run to run tests?',
+            default: 'npm test'
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'What does the user need to know about contributing to the repo?'
         }
-    ])
+    ]);
 };
