8c690326dc2982f767f3d32f64c2b8118917e9ca
devramirez <aramirezdev1@gmail.com>
2023-06-18 21:53:14 -0400

    added another prompt to create readme

--- index.js
+++ index.js
@@ -36,7 +36,22 @@ const questions = () => {
                 }
         
             },
+
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is your project name?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your project name!');
+                        return false; 
+                    }
+                }
+            },
             
+
         }
     ])
 }
